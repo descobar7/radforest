82fd4303a36a86c27632de6f0af04e4235e6cc6f
descobar7 <54652265+descobar7@users.noreply.github.com>
2019-10-17 16:28:12 -0400

    add initial folder structure
    Adding Initial folder strucuture with a fake test

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name = 'radforest',
+    version = '0.1.0',
+    description = 'This is the rad forest generator',
+    url ='',
+    author ='danEscobar',
+    author_email='',
+    licence='MIT',
+    packages=setuptools.find_packages(exclude=['tests'])
+)
---
+++ tests/sphere_test.py
@@ -0,0 +1,5 @@
+import radforest
+
+def test_sphere():
+    """There should e a real test here"""
+    assert 1 == 1
